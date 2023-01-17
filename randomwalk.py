083430e1708718572078ccbe90af010271276bb2
Aryan Rajesh <122169573+Aryan-Rajesh-Python@users.noreply.github.com>
2023-01-17 13:50:46 +0530

    Add files via upload

--- randomwalk.py
+++ randomwalk.py
@@ -7,9 +7,17 @@ colors=["red","green","blue","brown","pink","violet","yellow"]
 directions=[0,90,180,270]
 tortoise.pensize(15)
 tortoise.speed("fast")
-n=int(input("Enter the number of directions: "))
-for i in range(n):
-    tortoise.color(random.choice(colors))
-    tortoise.forward(30)
-    tortoise.setheading(random.choice(directions))
-screen.exitonclick()
+def random_walk():
+    n=int(input("Enter the number of directions: "))
+    for i in range(n):
+        tortoise.color(random.choice(colors))
+        tortoise.forward(30)
+        tortoise.setheading(random.choice(directions))
+    screen.exitonclick()
+while(True):
+    choice=input("Do you want to continue drawing the random walk: ")
+    if(choice=="yes"):
+        random_walk()
+    else:
+        print("Thank you for using our application!")
+        break
